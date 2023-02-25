93c5a4e46c605ed0e64659ffb23a9f9689290a79
Jaibw <jai.unix@gmail.com>
2023-02-25 09:45:44 +0530

    Update install-docker-ubuntu-20.04.sh

--- install-docker-ubuntu-20.04.sh
+++ install-docker-ubuntu-20.04.sh
@@ -5,4 +5,3 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
-sudo usermod -aG docker ubuntu
